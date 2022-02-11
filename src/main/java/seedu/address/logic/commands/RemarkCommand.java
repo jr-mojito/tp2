e6e2aa793372d52521172b68bc2e5d7b534f1fba
lim <crazy.hour.ai@gmail.com>
2022-02-11 16:37:07 +0800

    Add whitespace

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -3,7 +3,7 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
-public class RemarkCommand extends Command{
+public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
 
